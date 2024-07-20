002ab349b75bdfd819ea1d279512642f335163d8
Ömer Faruk Yüce <146741641+omerfarukyuce@users.noreply.github.com>
2024-07-20 18:09:17 +0300

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,12 @@
+void menu(){
+   printf("----------------------------------\n");
+   printf("*                                *\n");
+   printf("*      Telephone Directory       *\n");
+   printf("*                                *\n");
+   printf("*      1. Add Telephone Number   *\n"); 
+   printf("*      2. List Telephone Numbers *\n");
+   printf("*      3. Search                 *\n");
+   printf("*      4. Exit                   *\n");
+   printf("*                                *\n");
+   printf("----------------------------------\n");
+}
